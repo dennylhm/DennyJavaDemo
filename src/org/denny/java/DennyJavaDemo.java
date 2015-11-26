945fca1afeead47e9e6813aeef4259b75050142c
dennylhm <2339053961@qq.com>
2015-11-26 11:42:33 +0800

    Weclome To Java World for Dennylhm !

---
+++ src/org/denny/java/DennyJavaDemo.java
@@ -0,0 +1,13 @@
+package org.denny.java;
+
+public class DennyJavaDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Weclome To Java World, @dennylhm");
+	}
+
+}
